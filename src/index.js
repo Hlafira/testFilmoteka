a8f47438545d7fe5c663f6ad8ffb850eeb8fd76a
Hlafira <107553024+Hlafira@users.noreply.github.com>
2023-01-04 20:22:09 +0200

    Add firebase data

--- src/index.js
+++ src/index.js
@@ -15,16 +15,16 @@ import { loginModal } from './js/firebase/loginModal';
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 //import { firebaseConfig } from './js/firebase/fireBaseData';
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  deleteUser,
-  updateProfile,
-} from 'firebase/auth';
+// //import {
+//   getAuth,
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword,
+//   onAuthStateChanged,
+//   deleteUser,
+//   updateProfile,
+// } from 'firebase/auth';
 
 import { changeMode } from './js/changeMode/changeMode';
 // import { testFbDataBase } from './js/firebase/test';
 // testFbDataBase();
-console.log('change');
+console.log('change1');
