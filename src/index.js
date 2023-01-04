953372ffd9efb80dc001705ec8e3a19b08f19b87
Hlafira <107553024+Hlafira@users.noreply.github.com>
2023-01-04 20:42:38 +0200

    Add firebase data

--- src/index.js
+++ src/index.js
@@ -27,4 +27,4 @@ import { FbFilmsAuth } from './js/firebaseFilm/testAuth';
 import { changeMode } from './js/changeMode/changeMode';
 // import { testFbDataBase } from './js/firebase/test';
 // testFbDataBase();
-console.log('change3');
+console.log('change4');
--- src/js/firebaseFilm/testAuth.js
+++ src/js/firebaseFilm/testAuth.js
@@ -6,6 +6,11 @@ import {
   deleteUser,
   updateProfile,
 } from 'firebase/auth';
+
+import { renderLogin } from '../renderLogin/renderLogin';
+import { authMy } from './fbInit';
+import { fbFilmsData } from './fbFilms';
+import { returnMessage } from '../dataStorage/errorsMessage';
 export class FbFilmsAuth {
   constructor(user, language = 'en') {
     console.log('constructor');
